c31f4a3ae1b060f13e5650564f6a4ddb79da85c1
Cibe-ofc <113320411+Cibe-ofc@users.noreply.github.com>
2022-11-03 00:53:32 +0700

    Update info-gcbot.js

--- plugins/info-gcbot.js
+++ plugins/info-gcbot.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
  let handler = async(m, { conn }) => { 
 
          let pp = fs.readFileSync('./thumbnail.jpg')
-         let gc1 = 'https://chat.whatsapp.com/JkdB3gSCwCRGgOdCRfkAt1'
+         let gc1 = 'https://chat.whatsapp.com/B6NS1pwwv1s6Bf7ZrIv2Q2'
          let caption = `*Mʏ Gᴄ Oғғɪᴄɪᴀʟ*`
          await conn.sendButtonDoc(m.chat, caption, wm, 'Owner', '.owner', m, { contextInfo: { 
              externalAdReply: {
