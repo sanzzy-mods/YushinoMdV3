ed03375b047999f784078f431b1907027385614c
Cibe-ofc <113320411+Cibe-ofc@users.noreply.github.com>
2022-11-03 00:51:55 +0700

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,7 +1,7 @@
 import moment from 'moment-timezone'
 let handler = async (m, { conn, text, participants, isAdmin, isOwner }) => {
     let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
-    let bcbg = '-'
+    let bcbg = 'https://chat.whatsapp.com/B6NS1pwwv1s6Bf7ZrIv2Q2'
     conn.send3ButtonLoc(m.chat, bcbg, wm, `${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────','Menu', '.menu', 'Owner', '.owner', `\nKakek Gw Sugiono`, '.huuu', null,
  /*let m.reply(`${text ? `${text}\n` : ''}┌─「 Tag All 」\n` + users.map(v => '│◦❒ @' + v.replace(/@.+/, '')).join`\n` + '\n└────', null, */{
         mentions: users
